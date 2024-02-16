94159893f0ac82d2ea502fea8627024fbcc6a09d
Erki Aas <erki@veebkolm.ee>
2024-02-16 18:47:46 +0200

    Enable arm64 build

--- build.sh
+++ build.sh
@@ -22,12 +22,12 @@ LDFLAGS="-s -X github.com/prometheus/common/version.Version=${GITVERSION} \
 -X github.com/prometheus/common/version.BuildUser=${GITHUB_ACTOR} \
 -X github.com/prometheus/common/version.BuildDate=${TIME}"
 
-for OS in "darwin" "linux" "windows"; do
-    for ARCH in "amd64" "386"; do 
+for OS in "linux" "windows"; do
+    for ARCH in "amd64" "arm64" "386"; do
         echo "Building ${OS}/${ARCH} with version: ${GITVERSION}, revision: ${GITREVISION}, buildUser: ${GITHUB_ACTOR}"
         if [[ $OS == "windows" ]]; then
             GO111MODULE=on CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -ldflags "${LDFLAGS}" -tags 'netgo static_build' -a -o ".build/${OS}-${ARCH}/beat-exporter.exe"
-        else 
+        else
             GO111MODULE=on CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -ldflags "${LDFLAGS}" -tags 'netgo static_build' -a -o ".build/${OS}-${ARCH}/beat-exporter"
         fi
     done
